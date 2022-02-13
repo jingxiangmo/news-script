a892a3c41288c97a83131f7471bd39283fc95a47
Jingxiang Mo <jingxiangmo@gmail.com>
2022-02-13 11:46:03 -0500

    Second script completed

---
+++ Audery's Script/news_script.ps1
@@ -0,0 +1,32 @@
+# Open these URLS with in the browser with powershell
+
+# Forbes
+Start-Process http://www.forbes.com/
+# TMX Money
+Start-Process http://www.tmxmoney.com/
+# Entrepreneur.com
+Start-Process http://www.entrepreneur.com/
+# CTV
+Start-Process http://www.ctvnews.ca/
+# New York Times
+Start-Process http://www.nytimes.com/
+# Bloomberg
+Start-Process http://www.bloomberg.com/
+# Financial Post
+Start-Process http://www.financialpost.com/
+# Wall Street Journal
+Start-Process http://www.wsj.com/
+# Al Jazeera
+Start-Process http://www.aljazeera.com/
+# BBC
+Start-Process http://www.bbc.co.uk/
+# News Scientist
+Start-Process http://www.newscientist.com/
+# The Verge
+Start-Process http://www.theverge.com/
+# Wired
+Start-Process http://www.wired.com/
+# Delish
+Start-Process http://www.delish.com/
+# History Today
+Start-Process http://www.history.com/
