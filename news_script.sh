28261801c539b1ac3d9ff2df8a727ac29f8441cb
Jingxiang Mo <jingxiangmo@gmail.com>
2022-02-13 11:29:49 -0500

    Completed for Michael

---
+++ news_script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# This script is used to display news pages on browser on click.
+
+# =========[ OPEN THESE WEBPAGES IN THE SAME BROSWER]========= #
+
+# Japan Times
+open http://www.japantimes.co.jp/ http://www.nytimes.com/ 
+sleep 0.5
+# BNN Bloomberg
+open http://www.bloomberg.com/
+sleep 0.5 
+
+# International-Der Speigel
+open https://www.spiegel.de/international/
+sleep 0.5
+
+# The Rio Times
+open https://www.riotimesonline.com/
+sleep 0.5
+
+# The Straits Times 
+open http://www.straitstimes.com/
+sleep 0.5
+
+# Montreal Gazette
+open http://www.montrealgazette.com/
+sleep 0.5
+
+# The Punch (Nigeria)
+open http://www.punchng.com/
+sleep 0.5
+
+# Al Jazeera English
+open https://www.aljazeera.com/
+
+
+
+
+
+
