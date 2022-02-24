f1ba74cd6f7abcc85de3d45dfda1253bb99f6897
Jingxiang Mo <jingxiangmo@gmail.com>
2022-02-24 18:31:36 -0500

    new script

--- Jack's Script/news_script.command
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# shellcheck disable=SC2013
-for link in $(cat source_of_links.txt); do
-    open "$link" &
-    sleep 0.5
-done
--- Jack's Script/news_script.sh
+++ Jack's Script/news_script.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-# shellcheck disable=SC2013
-for link in $(cat source_of_links.txt); do
-    open "$link" &
-    sleep 0.5
-done
+# List of news sources
+open https://www.bbc.com/news
+sleep 0.5
+open https://www.aljazeera.com/
+sleep 0.5
+open https://apnews.com/
+sleep 0.5
+open https://www.theglobeandmail.com/
+sleep 0.5
+open https://www.al-monitor.com/
+sleep 0.5
+open https://foreignpolicy.com/channel/middle-east-africa/
+sleep 0.5
+open https://montrealgazette.com/
+sleep 0.5
+open https://www.journaldemontreal.com/
+sleep 0.5
+open https://ipolitics.ca/
+sleep 0.5
+open https://www.macleans.ca/
+sleep 0.5
+open https://www.mtlblog.com/
+sleep 0.5
+open https://www.cbc.ca/
+sleep 0.5
+open https://www.wsj.com/?mod=wsjheader_logo
+
+
